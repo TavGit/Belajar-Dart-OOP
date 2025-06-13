68f9e6b5b5620767022e6795dd2ac4cb7ff386cd
TavGit <gustavbaim18@gmail.com>
2025-06-13 14:07:01 +0700

    update materi super

--- super.dart
+++ super.dart
@@ -1,28 +1,41 @@
 class Kotak {
 
+  // ini field parent
   String name = "Pukimak";
+
+  // ini method parent
   int ambilKotak() {
     return 0;
   }
 }
 
 class Bulat extends Kotak {
-
+  // ini field child
   String name = "Acumalaka";
+
+  // ini method child
   int ambilKotak() {
     return 10;
   }
+
+  // buat method baru yg akan mengakses method yg berada di class parent
   int parentKotak() {
-    return super.ambilKotak(); 
+    return super.ambilKotak(); // > mengakses method yg barada di class parent
   }
+
+  // buat method baru yg akan mengakses field yg berada di class parent
   String parentField() { 
-    return super.name; 
+    return super.name; // > mengakses field yg berada di class parent
   }
 }
 
 void main() {
+  // buat object nya
   var bulat = Bulat();
+  // mengakses method child
   print("Method yg berada di class child = ${bulat.ambilKotak()}"); 
+
+  // mengakses method dan field yg berada di class parent
   print("Method yg berada di class parent = ${bulat.parentKotak()}");
   print("Field yg berada di class parent = ${bulat.parentField()}");
 }
