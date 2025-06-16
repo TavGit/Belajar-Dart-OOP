135de4dbb60277e82117b42eb38b277e2e08a49c
TavGit <gustavbaim18@gmail.com>
2025-06-16 11:30:51 +0700

    tambah materi import

---
+++ data/category.dart
@@ -0,0 +1,9 @@
+// INI HANYA CONTOH FILE SAJA
+// FILE INI AKAN DI IMPORT KE DALAM FILE import.dart
+
+class Category {
+  String id;
+  String name;
+
+  Category(this.id, this.name);
+}
---
+++ import.dart
@@ -0,0 +1,10 @@
+// Import
+
+// import folder data dan file category yg tdi sudah kita buat
+import 'data/category.dart';
+
+void main() {
+  var objectCategory = Category('1', 'Gustav');
+  print(objectCategory.id);
+  print(objectCategory.name);
+}
