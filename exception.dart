07a67ad8080858130d331f8cf951646443afc1a5
TavGit <gustavbaim18@gmail.com>
2025-07-17 21:32:24 +0700

    tambah materi exception

---
+++ exception.dart
@@ -0,0 +1,33 @@
+// Exception adalah cara untuk membuat sebuah pemberitahuan Error
+// kita bisa membuat exception sendiri atau menggunakan exception yg sudah disediakan oleh Dart
+// untuk membuat exception sendiri, kita bisa gunakan kata kunci "throw", lalu diikuti dengan object exception nya
+// contoh exception bawaan Dart
+// throw Exception();
+
+// Membuat class exception sendiri
+class ValidationException {
+  String? pesan;
+  ValidationException(this.pesan);
+}
+
+// Menggunakan exception yg sudah disediakan oleh Dart
+class Validation {
+  static void validate(String username, String password) {
+    if (username == "") {
+      throw ValidationException("Username tidak boleh kosong");
+      // throw Exception(
+      //   "Username tidak boleh kosong",
+      // ); // > ini exception bawaan nya Dart
+    } else if (password == "") {
+      throw ValidationException("Password tidak boleh kosong");
+      // throw Exception(
+      //   "Password tidak boleh kosong",
+      // ); // > ini exception bawaan nya Dart
+    }
+  }
+}
+
+// cara memanggil exception nya
+void main() {
+  Validation.validate("user", "");
+}
